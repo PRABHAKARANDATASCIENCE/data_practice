7ac7732e43ccc2f25443342e3ee01dbddda98560
itcmark4@gmail.com <itcmark4@gmail.com>
2026-09-10 12:39:32 +0000

    completed first SDE with SQL Practice
    created 2 tables with depend on other

--- SDP_Practice_with_sql/SDP_Practice_with_sql/transformations/my_transformation.sql
+++ SDP_Practice_with_sql/SDP_Practice_with_sql/transformations/my_transformation.sql
@@ -1,4 +1,10 @@
 CREATE MATERIALIZED VIEW `sample_trips_pipeline` as 
 SELECT 
 pickup_zip,
-fare_amount from samples.nyctaxi.trips
+fare_amount from samples.nyctaxi.trips;
+
+CREATE MATERIALIZED VIEW `sample_zones_pipleine` as
+select pickup_zip, 
+sum(fare_amount) as total_fare
+from sample_trips_pipeline
+group by pickup_zip;
